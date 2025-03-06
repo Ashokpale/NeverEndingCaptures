5f0c4eaf535b9466662ac65358105b6fb93a47bb
Ashokpale <paleashok47@gmail.com>
2025-03-06 16:27:02 +0530

    updated nav and humburger bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo-nec3.png'
+import React, { useState } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo-nec3.png';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false); // State to toggle menu
+
   return (
     <nav className='nav-container'>
-   <img src={logo} alt="Logo" style={{ width: "8%", height: "5%" }} />
-     <ul>
+      {/* Logo */}
+      <img src={logo} alt="Logo" className="logo" />
+
+      {/* Hamburger Menu Button */}
+      <button className="menu-btn" onClick={() => setIsOpen(!isOpen)}>
+        ☰
+      </button>
+
+      {/* Navigation Links */}
+      <ul className={isOpen ? "nav-links open" : "nav-links"}>
         <li>HOME</li>
         <li>Gallery</li>
         <li>About Us</li>
         <li><button className='btn'>Contact Us</button></li>
-     </ul>
+      </ul>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
